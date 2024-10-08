86b0a4a4b275c7272b05c709e3a9b5fb268f9e2d
alex-davis-ang <charles.leonard.com@gmail.com>
2024-10-08 15:42:23 +0900

    redeen added

--- contracts/ERC20Staking.sol
+++ contracts/ERC20Staking.sol
@@ -1,83 +1,78 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT  
+pragma solidity ^0.8.0;  
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";  
+import "@openzeppelin/contracts/utils/math/Math.sol";  
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";  // Use ReentrancyGuard  
 
-contract Staking {
-using Math for uint256;
+contract Staking is ReentrancyGuard {  
+    using Math for uint256;  
 
-IERC20 public stakingToken;
-uint256 public rewardRate; // Reward per minute
-uint256 public totalStaked;
-uint256 public lastUpdateTime;
+    IERC20 public stakingToken;  
+    uint256 public rewardRate; // Reward per minute  
+    uint256 public totalStaked;  
 
-struct Stake {
-    uint256 amount;
-    uint256 rewardDebt;
-    uint256 lastStakedTime;
-}
+    struct Stake {  
+        uint256 amount;  
+        uint256 rewardDebt;  
+        uint256 lastStakedTime;  
+    }  
 
-mapping(address => Stake) public stakes;
-mapping(address => uint256) public rewards;
+    mapping(address => Stake) public stakes;  
+    mapping(address => uint256) public rewards;  
 
-constructor(IERC20 _stakingToken, uint256 _rewardRate) {
-stakingToken = _stakingToken;
-rewardRate = _rewardRate;
-}
+    constructor(IERC20 _stakingToken, uint256 _rewardRate) {  
+        stakingToken = _stakingToken;  
+        rewardRate = _rewardRate;  
+    }  
 
-function stake(uint256 _amount) external {
-require(_amount > 0, "Cannot stake 0");
+    function stake(uint256 _amount) external nonReentrant {  
+        require(_amount > 0, "Cannot stake 0");  
 
-updateReward(msg.sender);
+        updateReward(msg.sender);  
 
-stakingToken.transferFrom(msg.sender, address(this), _amount);
+        stakes[msg.sender].amount += _amount;  
+        stakes[msg.sender].lastStakedTime = block.timestamp;  
 
-Stake storage userStake = stakes[msg.sender];
-userStake.amount = userStake.amount + _amount;
-userStake.rewardDebt = userStake.rewardDebt + _amount * rewardRate;
-userStake.lastStakedTime = block.timestamp;
+        totalStaked += _amount;  
 
-totalStaked = totalStaked + _amount;
-lastUpdateTime = block.timestamp;
-}
+        stakingToken.transferFrom(msg.sender, address(this), _amount);  
+    }  
 
-function withdraw(uint256 _amount) external {
-Stake storage userStake = stakes[msg.sender];
-require(userStake.amount >= _amount, "Withdraw amount exceeds staked amount");
+    function withdraw(uint256 _amount) external nonReentrant {  
+        Stake storage userStake = stakes[msg.sender];  
+        require(userStake.amount >= _amount, "Withdraw amount exceeds staked amount");  
 
-updateReward(msg.sender);
+        updateReward(msg.sender);  
 
-userStake.amount = userStake.amount - _amount;
-stakingToken.transfer(msg.sender, _amount);
+        userStake.amount -= _amount;  
+        totalStaked -= _amount;  
 
-totalStaked = totalStaked - _amount;
-lastUpdateTime = block.timestamp;
-}
+        stakingToken.transfer(msg.sender, _amount);  
+    }  
 
-function claimReward() external {
-updateReward(msg.sender);
+    function claimReward() external nonReentrant {  
+        updateReward(msg.sender);  
 
-uint256 reward = rewards[msg.sender];
-rewards[msg.sender] = 0;
+        uint256 reward = rewards[msg.sender];  
+        rewards[msg.sender] = 0;  
 
-stakingToken.transfer(msg.sender, reward);
-}
+        stakingToken.transfer(msg.sender, reward);  
+    }  
 
-function updateReward(address _user) internal {
-Stake storage userStake = stakes[_user];
+    function updateReward(address _user) internal {  
+        Stake storage userStake = stakes[_user];  
+        if (userStake.amount > 0) {  
+            uint256 reward = calculateReward(_user);  
+            rewards[_user] += reward;  
+            userStake.lastStakedTime = block.timestamp; // Update timestamp after calculation  
+        }  
+    }  
 
-if (userStake.amount > 0) {
-uint256 reward = calculateReward(_user);
-rewards[_user] = rewards[_user] + reward;
-userStake.rewardDebt = userStake.amount*(rewardRate)*(block.timestamp- userStake.lastStakedTime)/(60);
-}
-}
-
-function calculateReward(address _user) public view returns (uint256) {
-Stake storage userStake = stakes[_user];
-uint256 stakingDuration = block.timestamp - (userStake.lastStakedTime);
-uint256 pendingReward = userStake.amount*(rewardRate)*(stakingDuration)/(60);
-return pendingReward;
-}
+    function calculateReward(address _user) public view returns (uint256) {  
+        Stake storage userStake = stakes[_user];  
+        uint256 stakingDuration = block.timestamp - userStake.lastStakedTime;  
+        uint256 pendingReward = (userStake.amount * rewardRate * stakingDuration) / 60;  
+        return pendingReward;  
+    }  
 }
